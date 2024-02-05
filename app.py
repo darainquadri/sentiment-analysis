a3a9e2d40d9beb36d4c3d724504b6ca6df2cd97e
Syed Darain Quadri <darain.quadri@gmail.com>
2024-02-05 20:33:34 +0530

    Fixed persistent issues

--- app.py
+++ app.py
@@ -6,22 +6,16 @@ tfidf = joblib.load('tfidf_vectorizer.joblib')
 svm = joblib.load('svm_sentiment_model.joblib')
 lblenc = joblib.load('label_encoder.joblib')
 
-initial_text = '''
-After a bridge and a train engine, Bihar's list of bizarre thefts continued with an entire pond being "stolen". The pond vanished and had a hut built on it, leaving the locals baffled in Darbhanga district.'
-'''
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global initial_text
     if request.method == 'POST':
         text = request.form['news']
-        initial_text = text
         sentiment_result = applyModel(preprocess_text(text))
         return render_template('result.html', text=text, sentiment=sentiment_result)
 
-    return render_template('index.html', text=initial_text)
+    return render_template('index.html')
 
 def applyModel(text):
     text_tfidf = tfidf.transform([text])
